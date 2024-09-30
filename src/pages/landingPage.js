9d0c4e581dc04735cd7cd955647c357b3df294b4
unknown <karthikrocks005@gmail.com>
2024-10-01 01:19:52 +0530

    header layout setup and design

---
+++ src/pages/landingPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LandingPage = () => {
+  return (
+    <div className="w-full h-full grid place-items-center">
+      <h1>Landing Home Page</h1>
+    </div>
+  );
+};
+
+export default LandingPage;
